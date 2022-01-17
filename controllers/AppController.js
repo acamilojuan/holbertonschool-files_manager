fa3093bfbf7265a94d3c9441f048b2423837d213
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2022-01-17 18:34:59 -0500

    Update AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,22 +1,22 @@
-import RedisClient from '../utils/redis';
-import DBClient from '../utils/db';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 class AppController {
   static getStatus(request, response) {
     const status = {
-      redis: RedisClient.isAlive(),
-      db: DBClient.isAlive(),
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
     };
-    return response.status(200).send(status);
+    response.status(200).send(status);
   }
 
   static async getStats(request, response) {
     const stats = {
-      users: await DBClient.nbUsers(),
-      files: await DBClient.nbFiles(),
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
     };
-    return response.status(200).send(stats);
+    response.status(200).send(stats);
   }
 }
 
-module.exports = AppController;
+export default AppController;
